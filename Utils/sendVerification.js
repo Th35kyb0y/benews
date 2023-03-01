aae70c223bf14374567fb1f834c14b2c50195ee0
deepak mishra <38756178+Th35kyb0y@users.noreply.github.com>
2023-03-01 15:05:59 +0530

    Update sendVerification.js

--- Utils/sendVerification.js
+++ Utils/sendVerification.js
@@ -7,7 +7,7 @@ const CLIENT_ID = '168960810677-7i4c08lvfol75vj322lvgti1lbl86hk2.apps.googleuser
 const CLIENT_SECRET = "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"
 const REDIRECT_URI ='https://developers.google.com/oauthplayground'
 // const REFRESH_TOKEN="1//04fPt7p_j01wPCgYIARAAGAQSNwF-L9Ir0epRofxsdfhtq8aXRA10b4YWHMCAc7pakE8NnHYW-83J_SJYkOKCysi9wK1agiYvu84"
-const REFRESH_TOKEN= "1//04GqENhXBD_ttCgYIARAAGAQSNwF-L9IrDhKPjuklmv0UqpS3ZkNhOuQx6_3QNonMfB-vlg5lDkMlISlYoUcg7kQUXMu3Rga9414"
+const REFRESH_TOKEN="1//04aq0wgsx_6KiCgYIARAAGAQSNwF-L9Ir2g-vS7tm8kjOqiiIM0fSQZrXmixKUKKWze5TcFS2G8lktDcOJc7Q2ZRtjbWi1bXt5H4"
 
 
 // const oAuth2Client = new google.auth.OAuth2(CLIENT_ID,  CLIENT_SECRET, REDIRECT_URI)
@@ -15,7 +15,7 @@ const REFRESH_TOKEN= "1//04GqENhXBD_ttCgYIARAAGAQSNwF-L9IrDhKPjuklmv0UqpS3ZkNhOu
 
 
 // const accessToken = "ya29.a0AVvZVsoQfAUjXQ_eWVpmh1op0NCEZWY-00i3AADbkZg3NZg6IQqzaTDWWOS0808Qw-1sSYE25tQ7BUmV2Ir3Lmvsvgm_ZIXuQVGt5wyBH2ux-tPS7aOllJeTMv7J-cSVnLzzTIu-d56j9yDItySn15O_ck-qaCgYKAW0SARESFQGbdwaIhnZDRzCf_6kb8kIozghYiQ0163"
-const accessToken =  "ya29.a0AVvZVsoDHM92R5frmzotpi_BNU75FZeXhdUsRbgPhcFhXMKa3PLymoTlCdE0hgwtXHqvKEK3oCUjyYfQVzuI5j3SC9KWEtXt8kjYHaCB75xTDiElRu5hseq_mpyYupLZIDOnDdtfPY0lHk2DzB1DmgEqcOMfaCgYKAZ0SARESFQGbdwaIQU_IgofqIaeW3VzAbhYUvQ0163"
+const accessToken =  "ya29.a0AVvZVsr16xeNNmD62ztjOKGv1vQEoFIIEpSeIGagD7zcRed7mrn4ZR6PGcKWCRPMfoIJ6Jgkah_RcLOByGtI2z2iHDiRNGh70Rf1_o9dv-qGZ8d4Jzd45KqqxNtftJqG0na1mE_nR-wI9L1zYansRlLKDYdyaCgYKAWESARESFQGbdwaIH5BUAy4j-BRw1yE08qrDJQ0163"
 
 
 const transporter = nodemailer.createTransport({
